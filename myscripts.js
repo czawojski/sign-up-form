66f949a4ff1e4054220fba100209a4ab9e5b065d
Christian Zawojski <clzawojski@gmail.com>
2023-12-17 14:59:40 -0500

    final form files

--- myscripts.js
+++ myscripts.js
@@ -1,9 +1,115 @@
+// There are many ways to pick a DOM node; here we get the form itself and the email
+// input box, as well as the span element into which we will place the error message.
+const form = document.querySelector("form");
 const email = document.getElementById("email");
+const emailError = document.querySelector("#email + span.error");
 
-email.addEventListener("mouseout", (event) => {
-  if (email.validity.typeMismatch) {
-    email.setCustomValidity("I am expecting an email address!");
+email.addEventListener("input", (event) => {
+  // Each time the user types something, we check if the
+  // form fields are valid.
+
+  if (email.validity.valid) {
+    // In case there is an error message visible, if the field
+    // is valid, we remove the error message.
+    emailError.textContent = ""; // Reset the content of the message
+    emailError.className = "error"; // Reset the visual state of the message
   } else {
-    email.setCustomValidity("");
+    // If there is still an error, show the correct error
+    showEmailError();
   }
 });
+
+form.addEventListener("submit", (event) => {
+  // if the email field is valid, we let the form submit
+  if (!email.validity.valid) {
+    // If it isn't, we display an appropriate error message
+    showEmailError();
+    // Then we prevent the form from being sent by canceling the event
+    event.preventDefault();
+  }
+});
+
+function showEmailError() {
+  if (email.validity.valueMissing) {
+    // If the field is empty,
+    // display the following error message.
+    emailError.textContent = "You need to enter an email address.";
+  } else if (email.validity.typeMismatch) {
+    // If the field doesn't contain an email address,
+    // display the following error message.
+    emailError.textContent = "Entered value needs to be an email address.";
+  } else if (email.validity.tooShort) {
+    // If the data is too short,
+    // display the following error message.
+    emailError.textContent = `Email should be at least ${email.minLength} characters; you entered ${email.value.length}.`;
+  }
+
+  // Set the styling appropriately
+  emailError.className = "error active";
+}
+
+
+
+const firstName = document.getElementById("first-name");
+const firstNameError = document.querySelector("#first-name + span.error");
+
+firstName.addEventListener("input", (event) => {
+  if (firstName.validity.valid) {
+    firstNameError.textContent = ""; 
+    firstNameError.className = "error"; 
+  } else {
+    showFirstnameError();
+  }
+});
+
+form.addEventListener("submit", (event) => {
+  if (!firstName.validity.valid) {
+    showFirstnameError();
+    event.preventDefault();
+  }
+});
+
+function showFirstnameError() {
+  if (firstName.validity.valueMissing) {
+    firstNameError.textContent = "You need to enter your first name.";
+  } else if (firstName.validity.typeMismatch) {
+    firstNameError.textContent = "Enter your first name.";
+  } else if (firstName.validity.tooShort) {
+    firstNameError.textContent = `First name should be at least ${firstName.minLength} characters; you entered ${firstName.value.length}.`;
+  }
+
+  firstNameError.className = "error active";
+}
+
+
+
+const lastName = document.getElementById("last-name");
+const lastNameError = document.querySelector("#last-name + span.error");
+
+lastName.addEventListener("input", (event) => {
+  if (lastName.validity.valid) {
+    lastNameError.textContent = ""; 
+    lastNameError.className = "error"; 
+  } else {
+    showLastnameError();
+  }
+});
+
+form.addEventListener("submit", (event) => {
+  if (!lastName.validity.valid) {
+    showLastnameError();
+    event.preventDefault();
+  }
+});
+
+function showLastnameError() {
+  if (lastName.validity.valueMissing) {
+    lastNameError.textContent = "You need to enter your last name.";
+  } else if (lastName.validity.typeMismatch) {
+    lastNameError.textContent = "Enter your last name.";
+  } else if (lastName.validity.tooShort) {
+    lastNameError.textContent = `First name should be at least ${lastName.minLength} characters; you entered ${lastName.value.length}.`;
+  }
+
+  lastNameError.className = "error active";
+}
